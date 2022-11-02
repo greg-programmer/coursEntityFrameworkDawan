5bff04d3e88b0f5ab550de1dd63df5cbd9afc511
Grégory Schoemaecker <91738933+greg-programmer@users.noreply.github.com>
2022-11-02 20:10:39 +0100

    Add files via upload

---
+++ testMethode/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testMethode
+{
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NombreDeChaine("fdfd"));
+            Console.WriteLine(NombreDeMotParChaine("dsds"));
+            Console.ReadKey();
+
+        }
+        public static int NombreDeChaine(string a)
+        {
+            return a.Length;
+        }
+        public static int NombreDeMotParChaine(string a)
+        {
+            string[]array = new string[a.Length];
+            return array.Length;
+        }
+    }
+}
